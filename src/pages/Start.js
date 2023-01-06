3c1be4b972dff419c562ae6e4eb11aa686cd58b2
ziiemli <zilin.moulin@outlook.fr>
2023-01-06 17:39:03 +0100

    first commit

---
+++ src/pages/Start.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Start = () => {
+    return (
+        <div>
+            <h1 className='.start_h1'>ZIIEMLI</h1>
+            <h2 className='.start_h2'>Zilin Moulin</h2>
+            <ul>
+                <li>Artist</li>
+                <li>Visual Designer</li>
+                <li>Front-end Developer</li>
+            </ul>
+        </div>
+    );
+};
+
+export default Start;
