9ce49478660492b8ce8a14768103db87df68b6d1
ziiemli <zilin.moulin@outlook.fr>
2023-01-09 17:18:18 +0100

    start page mobile okay

--- src/pages/Start.js
+++ src/pages/Start.js
@@ -1,10 +1,14 @@
 import React from "react"
 import {Link} from "react-router-dom"
 import Photography from "../assets/img/start/Photography_Background.png"
+import Logo from "../assets/Logo.png"
 
 const Start = () => {
     return (
         <div className="start">
+            <div className="start__logo">
+                <img src={Logo} alt="Logo of Ziiemli" />
+            </div>
             <div className="start__content">
                 <h1 className="start__content__h1">ZIIEMLI</h1>
                 <h2 className="start__content__h2">Zilin Moulin</h2>
@@ -13,9 +17,13 @@ const Start = () => {
                     <li>Visual Designer</li>
                     <li>Front-end Developer</li>
                 </ul>
+            </div>
+            <div className="start__button">
+                <div className="start__button__line"></div>
                 <Link to={"home"}>
-                    <h2 className="start__content__start">START</h2>
+                    <h2 className="start__button__start">START</h2>
                 </Link>
+                <div className="start__button__line"></div>
             </div>
             <div className="start__background">
                 <img className="start_photography_background" src={Photography} alt="Photography of Zilin Moulin" />
