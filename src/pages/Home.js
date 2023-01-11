8044dd37f4bb906d17135f5ccbde01ee6c4e5383
ziiemli <zilin.moulin@outlook.fr>
2023-01-11 22:22:12 +0100

    Home Mobile OK

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -17,22 +17,22 @@ const Home = () => {
 
     return (
         <div>
-            {/* <Header /> */}
+            <Header />
             <main className="homeCategories">
-                <h1>POUET</h1>
                 {categories && categories.map((category) => (
-                    <NavLink to={category.nav} className="nav">
-                        <article>
-                            <h2>{category.category}</h2>
+                    <NavLink to={category.nav} className="nav" key={category.id}>
+                        <article className="homeCategories__article">
+                            <h2 className="homeCategories__article__title">{category.category}</h2>
+                            <div className="homeCategories__article__gradient"></div>
                             <img src={category.image} alt={"image to illustrate the " + category.category + " category"} />
                         </article>
                     </NavLink>
                     
                 ))}
             </main>
-            {/* <div className="backgroundHome">
+            <div className="backgroundHome">
                 <img src={backgroundHome} alt="Gray background with soft forms in translucent" />
-            </div> */}
+            </div>
         </div>
     )
 }
