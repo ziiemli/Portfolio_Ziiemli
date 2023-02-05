6c5521fb27f1e0d4f121e2f8ea63e314a77f6af2
ziiemli <zilin.moulin@outlook.fr>
2023-02-05 17:43:11 +0100

    correction: function next project, add if there are not next project to get back first project

--- src/components/ProjectsNext.js
+++ src/components/ProjectsNext.js
@@ -10,14 +10,31 @@ const ProjectsNext = ({data, dataProjects}) => {
     const nextProject = () => {
         //next Index of Array
         const indexNextArray = dataProjects[indexActualProject+1]
-        //find name of next Project
-        const nextName = indexNextArray && indexNextArray.nameProject
-        //delete space of nameProject
-        const urlName =  nextName.split(' ').join('_')
-        //delete accents
-        const urlNameNoAccents =  urlName.normalize("NFD").replace(/[\u0300-\u036f]/g, "")
-        //redirection to the next Project
-        window.location.href = "/Web_Development_and_Design/projects/" + urlNameNoAccents;
+        //find the number of actual array
+        const numberOfArray = indexNextArray && dataProjects.indexOf(indexNextArray)
+        //if length of table data > actual number of array = next page
+        if(dataProjects.length > numberOfArray) {
+            //find name of next Project
+            const nextName = indexNextArray && indexNextArray.nameProject
+            //delete space of nameProject
+            const urlName =  nextName.split(' ').join('_')
+            //delete accents
+            const urlNameNoAccents =  urlName.normalize("NFD").replace(/[\u0300-\u036f]/g, "")
+            //redirection to the next Project
+            window.location.href = "/Web_Development_and_Design/projects/" + urlNameNoAccents;
+        }
+        //else return to first project
+        else {
+            const indexFirstArray = dataProjects[0]
+            const indexFirstArrayName = indexFirstArray && indexFirstArray.nameProject
+            //delete space of nameProject
+            const urlNameFirstArray = indexFirstArrayName.split(' ').join('_')
+            //delete accents
+            const urlNameFirstArrayNoAccents =  urlNameFirstArray.normalize("NFD").replace(/[\u0300-\u036f]/g, "")
+            //redirection to the next Project
+            window.location.href = "/Web_Development_and_Design/projects/" + urlNameFirstArrayNoAccents;
+
+        }
     }
 
     return (
