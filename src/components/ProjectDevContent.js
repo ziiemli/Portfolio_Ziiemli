a2c5702789189baee474c676c03e4eb81bf2629b
ziiemli <zilin.moulin@outlook.fr>
2023-02-06 17:41:25 +0100

    add screens et change img on json file

--- src/components/ProjectDevContent.js
+++ src/components/ProjectDevContent.js
@@ -2,19 +2,27 @@ import React from "react"
 import Screen from "../components/Screen"
 
 const ProjectDevContent = ({data}) => {
-
     //recover url
     const querystring = window.location.pathname.split("/Web_Development_and_Design/projects/").join("")
     //recover name
     const name = querystring.split("_").join(" ")
     //find good data of project
-    const dataContent = data && data.find((el) => el.nameProject === name)
-    console.log(dataContent && dataContent);
+    const dataContent = data && data.find((el) => el.nameProject.normalize("NFD").replace(/[\u0300-\u036f]/g, "") === name)
+    console.log(dataContent && dataContent)
     return (
         <section className="projectDevContent">
-            <Screen title={"HOME"} img={dataContent && dataContent.home} projectName={dataContent && dataContent.nameProject}/>
-            <Screen title={"ARTICLE"} img={dataContent && dataContent.article} projectName={dataContent && dataContent.nameProject}/>
-            <Screen title={"ABOUT"} img={dataContent && dataContent.about} projectName={dataContent && dataContent.nameProject}/>
+            {/* HOME PAGE */}
+            {dataContent && dataContent.home && <Screen title={"HOME"} img={dataContent.home} projectName={dataContent.nameProject} />}
+            {/* ARTICLE PAGE */}
+            {dataContent && dataContent.article && <Screen title={"ARTICLE"} img={dataContent.article} projectName={dataContent.nameProject} />}
+            {/* ABOUT PAGE */}
+            {dataContent && dataContent.about && <Screen title={"ABOUT"} img={dataContent.about} projectName={dataContent.nameProject} />}
+            {/* CART PAGE (kanap) */}
+            {dataContent && dataContent.cart && <Screen title={"CART"} img={dataContent.cart} projectName={dataContent.nameProject} />}
+            {/* CONTACT PAGE (LaPanthere) */}
+            {dataContent && dataContent.contact && <Screen title={"CONTACT"} img={dataContent.contact} projectName={dataContent.nameProject} />}
+            {/* MENU PAGE (OhMyFood) */}
+            {dataContent && dataContent.menu && <Screen title={"MENU"} img={dataContent.menu} projectName={dataContent.nameProject} />}
         </section>
     )
 }
