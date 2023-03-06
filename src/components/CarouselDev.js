fa1491fec83083ed7ad65b478e3b77478e2a8b79
ziiemli <zilin.moulin@outlook.fr>
2023-03-06 18:50:18 +0100

    carouselDev: add prevProject and completed json file

--- src/components/CarouselDev.js
+++ src/components/CarouselDev.js
@@ -1,20 +1,10 @@
-import React, {useState} from "react"
+import React from "react"
 
 const CarouselDev = ({dataProjects, data}) => {
-    // console.log(data)
-    // const [current, setCurrent] = useState(0)
-    // const picturesLength = dataProjects && dataProjects.length
+
     // const pictures = dataProjects
     const rightArrow = process.env.PUBLIC_URL + "/assets/Right_Arrow.svg"
     const leftArrow = process.env.PUBLIC_URL + "/assets/Left_Arrow.svg"
-
-    // // const nextSlide = () => {
-    // //     setCurrent(current === picturesLength - 1 ? 0 : current + 1)
-    // // }
-
-    // const prevSlide = () => {
-    //     setCurrent(current === 0 ? picturesLength - 1 : current - 1)
-    // }
 
     //name of Actual Project
     const nameOfProject = data && data.nameProject
@@ -49,22 +39,52 @@ const CarouselDev = ({dataProjects, data}) => {
             window.location.href = "/Web_Development_and_Design/projects/" + urlNameFirstArrayNoAccents
         }
     }
+
+    const prevProject = () => {
+        //next Index of Array
+        const indexPrevArray = dataProjects[indexActualProject - 1 ]
+        console.log(indexPrevArray);
+        // find the number of actual array
+        const numberOfArray = indexPrevArray && dataProjects.indexOf(indexPrevArray)
+        //if length of table data > actual number of array = next page
+        if (dataProjects.length > numberOfArray) {
+            //find name of next Project
+            const prevName = indexPrevArray && indexPrevArray.nameProject
+            //delete space of nameProject
+            const urlName = prevName.split(" ").join("_")
+            //delete accents
+            const urlNameNoAccents = urlName.normalize("NFD").replace(/[\u0300-\u036f]/g, "")
+            //redirection to the next Project
+            window.location.href = "/Web_Development_and_Design/projects/" + urlNameNoAccents
+        }
+        // //else return to first project
+        else {
+            const indexLastArray = dataProjects[dataProjects.length - 1]
+            const indexLastArrayName = indexLastArray && indexLastArray.nameProject
+            //delete space of nameProject
+            const urlNameLastArray = indexLastArrayName.split(" ").join("_")
+            //delete accents
+            const urlNameLastArrayNoAccents = urlNameLastArray.normalize("NFD").replace(/[\u0300-\u036f]/g, "")
+            //redirection to the next Project
+            window.location.href = "/Web_Development_and_Design/projects/" + urlNameLastArrayNoAccents
+        }
+    }
+    
     const arrowBack = process.env.PUBLIC_URL + "/assets/ArrowBack.svg"
     const projectNumber = indexActualProject && indexActualProject === 0 ? 1 : indexActualProject+1
-    console.log(projectNumber);
     const currentProjectIndicate = dataProjects && projectNumber && ` 0${projectNumber} / 0${dataProjects.length} `
 
     return (
         <section className="carouselDev">
-            <img className="carouselDev__leftArrow" src={leftArrow} alt="Flèche en direction de la gauche" onClick={nextProject} />
+            <img className="carouselDev__leftArrow" src={leftArrow} alt="Flèche en direction de la gauche" onClick={prevProject} />
             <img className="carouselDev__rightArrow" src={rightArrow} alt="Flèche en direction de la droite" onClick={nextProject} />
             <div>
                 <h1 className="carouselDev__title">{data && data.nameProject}</h1>
-                <img className="carousel__slide" src={data && data.thumbnail} alt={"image"} />
-                <img className="thumbnailDesktopBackground" src={data && data.thumbnailBackground} alt={"image"} />
+                <img className="carouselDev__slide" src={data && data.thumbnail} alt={"image Thumbnail of " + data && data.nameProject} />
+                <img className="thumbnailDesktopBackground" src={data && data.thumbnailBackground} alt={"image Background of " + data && data.nameProject} />
                 <div className="carouselDev__back">
                     <a href="/Web_Development_and_Design">
-                        <img src={arrowBack && arrowBack} />
+                        <img src={arrowBack && arrowBack} alt="left arrow"/>
                         <p>Projects</p>
                     </a>
                     <p>{currentProjectIndicate}</p>
