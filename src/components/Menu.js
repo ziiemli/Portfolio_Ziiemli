faebd76509688d776ac377e575fa2981aebea54d
ziiemli <zilin.moulin@outlook.fr>
2023-03-11 00:15:27 +0100

    adjust responsive desktop for menu page

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -7,18 +7,6 @@ const Menu = () => {
     //menu is active ?
     const [menu, setMenu] = useState(false)
 
-    // // homeCategories Height disable
-    // //select ID homeCategoriesHeight
-    // const homeCategoriesHeight = document.getElementById("homeCategoriesHeight")
-    // //if menu false > height 110%
-    // if (!menu) {
-    //     homeCategoriesHeight && homeCategoriesHeight.classList.add("heightHomeCategories")
-    // }
-    // //if menu true > height disable
-    // else {
-    //     homeCategoriesHeight && homeCategoriesHeight.classList.remove("heightHomeCategories")
-    // }
-
     //no scroll when menu is open
     //select body tag
     const bodyContent = document.querySelector("body")
@@ -46,7 +34,7 @@ const Menu = () => {
                 <section className="menu__active">
                     <div className="menu__active__nav">
                         <NavLink className="menu__active__nav__shop">
-                            {querystring && querystring === "shop" && <img className="menu__active__nav__diamond" src={process.env.PUBLIC_URL + "/assets/Diamond.svg"} alt="losage select" />}
+                            {/* {querystring && querystring === "shop" && <img className="menu__active__nav__diamond" src={process.env.PUBLIC_URL + "/assets/Diamond.svg"} alt="losage select" />} */}
                             <p>Shop</p>
                         </NavLink>
                         <NavLink  to="/home" className="menu__active__nav__page">
@@ -54,7 +42,7 @@ const Menu = () => {
                             <p>Home</p>
                         </NavLink>
                         <NavLink to="/About" className="menu__active__nav__page">
-                            {querystring && querystring === "about" && <img className="menu__active__nav__diamond" src={process.env.PUBLIC_URL + "/assets/Diamond.svg"} alt="losage select" />}
+                            {querystring && querystring === "About" && <img className="menu__active__nav__diamond" src={process.env.PUBLIC_URL + "/assets/Diamond.svg"} alt="losage select" />}
                             <p>About</p>
                         </NavLink>
                         <NavLink className="menu__active__nav__page">
