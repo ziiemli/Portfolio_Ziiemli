70a86320a375f2543c16a737d00dc2487257a94c
ziiemli <zilin.moulin@outlook.fr>
2023-12-06 17:54:14 +0100

    add gsap animation for CategoriesTitle

--- src/components/CategoriesTitle.js
+++ src/components/CategoriesTitle.js
@@ -1,6 +1,55 @@
 import React from "react"
+import gsap from "gsap";
+import { useEffect } from "react";
 
 const CategoriesTitle = ({image, imageDesktop, title, paragraphe}) => {
+    
+    //gsap animation
+    useEffect(() => {
+        //group of informations
+        gsap.fromTo(".categoriesTitle", {
+          opacity: 0,
+          ease: "power2.out",
+        }, {
+            opacity:1
+        });
+        
+        //title
+        gsap.fromTo(".categoriesTitle__informations__title", {
+          opacity: 0,
+          x: -50,
+          duration: 1,
+          ease: "power2.out",
+        },{
+            opacity:1,
+            x: 0
+        });
+    
+        //paragraphe
+        gsap.fromTo(".categoriesTitle__informations__paragraphe", {
+            opacity: 0,
+            x: -50,
+            duration: 1,
+            ease: "power2.out",
+        },{
+            opacity:1,
+            x: 0,
+            delay: 0.2
+          });
+
+        //line
+        gsap.fromTo(".categoriesTitle__line", {
+            opacity: 0,
+            y: -600,
+            duration: 1,
+            ease: "power2.out",
+        },{
+            opacity:1,
+            y: 0,
+            delay: 0.1
+          });
+      }, []);
+
     return (
         <section className="categoriesTitle" id="test">
             <picture className="categoriesTitle__img">
