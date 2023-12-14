53783a3cd9ca62e3b2cddcaebf7cb6e28216d7cb
ziiemli <zilin.moulin@outlook.fr>
2023-12-14 01:07:11 +0100

    Gallery features ok

--- src/components/GalleryPhotos.js
+++ src/components/GalleryPhotos.js
@@ -1,14 +1,36 @@
-import React from "react"
+import React, {useState} from "react"
 import Gallery from "react-photo-gallery"
 
 const GalleryPhotos = ({photos}) => {
+    //data photos
     const images = photos && photos
+
+    const rightArrow = process.env.PUBLIC_URL + "/assets/Right_Arrow.svg"
+    const leftArrow = process.env.PUBLIC_URL + "/assets/Left_Arrow.svg"
+
+    const [selectedPhotoIndex, setSelectedPhotoIndex] = useState()
+
+    const openPhoto = (index) => {
+        setSelectedPhotoIndex(index)
+        console.log(selectedPhotoIndex)
+        console.log(index)
+        console.log(images[index])
+    }
+
+    const closePhoto = () => {
+        setSelectedPhotoIndex()
+    }
+
+    const navigatePhotos = (direction) => {
+        const newIndex = (selectedPhotoIndex + direction + photos.length) % photos.length
+        setSelectedPhotoIndex(newIndex)
+    }
 
     return (
         <div className="gallery">
             {images &&
                 images.map((image, index) => (
-                    <div className="gallery__img" key={index}>
+                    <div className="gallery__img" key={index} onClick={() => openPhoto(index)}>
                         <img src={image.src} alt={image.description} />
                         <div className="gallery__img__infos">
                             <h3>{image.serie}</h3>
@@ -20,6 +42,14 @@ const GalleryPhotos = ({photos}) => {
                         </div>
                     </div>
                 ))}
+            {selectedPhotoIndex !== undefined && images && (
+                <div className="galleryPhotoModal">
+                    <img className="galleryPhotoModal__arrow" src={leftArrow} alt="left Arrow" onClick={() => navigatePhotos(-1)}/>
+                    <img className="galleryPhotoModal__img" src={images[selectedPhotoIndex].src} alt="Selected Photo" />
+                    <img className="galleryPhotoModal__arrow" src={rightArrow} alt="right Arrow" onClick={() => navigatePhotos(1)}/>
+                    <div className="galleryPhotoModal__background" onClick={() => closePhoto()}></div>
+                </div>
+            )}
         </div>
     )
 }
